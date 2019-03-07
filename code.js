3c972d8d847692525011f9ae00885a939e52b12b
jepidoptera <jepidoptera@live.com>
2019-03-06 20:08:19 -0600

    working on logic

--- code.js
+++ code.js
@@ -1,7 +1,28 @@
 // jshint esversion: 6
+class employee {
+    constructor (name, rate, start) {
+        this.name = name;
+        this.rate = rate;
+        this.start = start;
+    }
+}
+
 $(document).ready(() => {
     // set up database
-    
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBtjv79c8tKZJgkr8QO0ResNvOd2oBVmKM",
+        authDomain: "employee-management-46a35.firebaseapp.com",
+        databaseURL: "https://employee-management-46a35.firebaseio.com",
+        projectId: "employee-management-46a35",
+        storageBucket: "employee-management-46a35.appspot.com",
+        messagingSenderId: "4136533109"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+    var employees = [];
+
     // set up colum names
     $("#people").append(
         $("<tr>")
@@ -15,6 +36,24 @@ $(document).ready(() => {
 
     // adding a new person
     $("#addperson").on("click", () => {
+        // Don't refresh the page!
+        event.preventDefault();
+
+        // YOUR TASK!!!
+        // Code in the logic for storing and retrieving the most recent user.
+        // Don't forget to provide initial data to your Firebase database.
+        name = $("#nameInput").val().trim();
+        rate= $("#rateInput").val().trim();
+        startDate = $("#dateInput").val().trim();
+        submitButton = $("#addperson").val().trim();
+
+        database.ref().push({
+        name: name,
+        rate: rate,
+        startDate: startDate,
+        
+        });
+        
         $("#people").append(
             $("<tr>")
                 .append(
@@ -24,5 +63,11 @@ $(document).ready(() => {
                     $("<td>").text("lifetime earnings")
                 )
         );
+    });
+
+    database.ref.on()('child_added', (newchild)=> {
+        var employee = new employee(newchild.val().name, newchild.val().rate, newchild.val().start);
+        employees.push(employee);
+        // add to table
     });
 });
